67f460a774999e5d86c4a21c29b11acc174635a8
LTHCTheMaster <mistermega52@gmail.com.com>
2022-01-19 09:57:34 +0100

    SOme optimizations

--- proportion.py
+++ proportion.py
@@ -2,72 +2,38 @@ from math import sqrt
 
 msg = 'BWFWKSAKWFIMWDKWFKDWKHZADGKGHZWKGFLKMHHGKWIMWDSESLAWJWWLSALAFVAXXWJWFLWSMEGMNWEWFLWLSMJWHGKUWIMADQSVWTAWFUWJLSAFUWKLIMWLGMKDWKUGJHKYJSNALWFLDWKMFKKMJDWKSMLJWKUWKLIMWLGMLWKDWKHSJLAUMDWKVWKUGJHKYJSNALWFLDWKMFWKKMJDWKSMLJWKUWKLIMWVSFKUWLMFANWJKLGMLWKLWFLJSFKDSLAGFGMAFFAKMGMWFLJSFKDSLAGFWLAFFAKMSDSXGAKUWLLWKMHHGKALAGFVWKHZADGKGHZWKJWKKWETDWHWMLWLJWSUWDDWVWKYWGEWLJWKIMASVEWLLWFLVWKHGAFLKKSFKSMUMFWVAEWFKAGFVWKDAYFWKKSFKDSJYWMJFAHJGXGFVWMJVWKKMJXSUWKKSFKWHSAKKWMJGMHWMLWLJWHSJDWFLADKVMJWHGKJWDSLAXVMFWESKKWSMFWSMLJWLGMLWKLVSFKMFJWHGKJWDSLAXWFMFNSAKKWSMTSLLMHSJDSLWEHWLWJAWFFQWKLWFMFJWHGKSTKGDMHSKEWEWDWKEGDWUMDWKSYJWYSLANWKFAVMNSAKKWSMFAVWKUGJHKIMADJWFXWJEWKADKFWUGFUGANWFLHSKHDMKVWLWFVSFUWSMJWHGKIMSMEGMNWEWFLVSFKMFUGJHKIMWDUGFIMWUWKLIMSHHSJWEEWFLADKJWYSJVWFLDSESLAWJWUGEEWZGEGYWFWUWKLIMADKXGFLSTKLJSULAGFVWLGMLWKDWKIMSDALWKIMADMAKGFLWKKWFLAWDDWKUWKLIMADKDSUGFKAVWJWFLUGEEWAFSDLWJSTDWVSFKDAFKLSFLHJWKIMWAFVANAKATDWVWDWMJKHWUMDSLAGFUWKLIMADKJSAKGFFWFLVMJWHGKJWDSLAXVMFSYJWYSLSMFSMLJWSYJWYSLUWKLIMADKGMTDAWFLIMWLSFVAKIMADKJSAKGFFWFLVWDAFVAXXWJWFUWVMUGJHKSMEGMNWEWFLGMSMJWHGKDWTDGUVWESJTJWLWFVSKSVAKKGDMLAGFUWKLIMADKSFWSFLAKKWFLHSJDSHWFKWWWLDWEGMNWEWFLYWFWJSDIMASFAEWLGMKDWKUGJHKWLDWMJSULAGFHSJLAUMDAWJWVWKMFKKMJDWKSMLJWKIMADWKVWLJMALLGMKUWKLIMWUWLLWAFVAXXWJWFUWIMGAIMWXSMKKWWFWDDWEWEWESAKEGEWFLSFWWFWJWFVJSHSKDWKDGAKVMEGMNWEWFLWJJGFWWK'
 
-def tri(dic):
-    """
-    Remet le dictionnaire dic dan l'ordre de la valeur plus grande proportion à la plus petite
-    """
-    dout = {}
-    temp = []
-    for i in dic:
-        temp.append(dic[i])
-    temp.sort(reverse=True)
-    for i in temp:
-        for j in dic:
-            if dic[j] == i:
-                dout[j] = i
-                break
-    return dout
+def decrypt(msg):
+    analysed = {} # Créer le dictionnaire des fréquences dans le texte codé
 
-def analyse(msg):
-    """
-    Donne la fréquence de chaque lettre dans msg
-    """
-    out = {}
-    n = len(msg)
+    # Boucler le message pour créer les proportions
     for i in msg:
-        if i not in out:
-            out[i] = 1
-        else:
-            out[i] += 1
-    for i in out:
-        out[i] = round(out[i] / n * 100, 2)
-    temp = tri(out)
-    return temp
+        if i not in analysed:
+            analysed[i] = 0
+        analysed[i] += 1
+    for i in analysed:
+        analysed[i] = round(analysed[i] / len(msg) * 100, 2)
+    
+    e_prop = 17.26 # Proportion réelle du "E" dans la langue française
 
-def nearest(chare, analysed):
-    """
-    Détermine quelle lettre est associé au E
-    """
+    # Obtention d'une correspondance avec le "E"
     char = ''
     dist = 0
     for i in analysed:
-        if sqrt(chare**2+analysed[i]**2)>dist:
-            dist = sqrt(chare**2+analysed[i]**2)
+        if sqrt(e_prop**2+analysed[i]**2)>dist:
+            dist = sqrt(e_prop**2+analysed[i]**2)
             char = i
-    return char
+    
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" # Alphabet français
+    d = {chr((i+alphabet.find(char))%26+65):chr((i+4)%26+65) for i in range(26)} # Création du dictionnaire des correspondances
 
-def scan(msg):
-    """
-    Fait un scan adapté au message qui en clair sont en français
-    """
-    analysed = analyse(msg)
-    real = {"E": 17.26, "A": 8.40, "S": 8.08, "I": 7.34, "N": 7.13, "T": 7.07, "R": 6.55, "L": 6.01, "U": 5.74, "O": 5.26, "D": 4.18, "C": 3.03, "P": 3.01, "M": 2.96, "V": 1.32, "G": 1.27, "F": 1.12, "B": 1.06, "Q": 0.99, "H": 0.92, "X": 0.45, "J": 0.31, "Y": 0.30, "Z": 0.12, "K": 0.05, "W": 0.04}
-    temp = nearest(real["E"],analysed)
-    return temp
-
-def decrypt(msg):
-    """
-    Déchiffre avec un code de césar après avoir récupéré la correspondance du E
-    """
-    e_cor = scan(msg)
-    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    d = {chr((i+alphabet.find(e_cor))%26+65):chr((i+4)%26+65) for i in range(26)}
+    # Décodage
     out = ''
     for i in msg:
         if i in d:
             out += d[i]
         else:
             out += i
+    
     return out
 
 print(decrypt(msg))
